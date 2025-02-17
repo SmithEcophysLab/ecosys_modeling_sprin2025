09bcb815c22607c20435c7bf7d0cb7674e24e5dd
Pawan Devkota <ddevkota.pawan@gmail.com>
2025-02-17 13:05:37 -0600

    add description to soil carbon module

---
+++ class_model/modules/devkota_module/devkota_soilcarbon.R
@@ -0,0 +1,34 @@
+# Title: soil carbon module for carbon cycle
+# Author: Pawan Devkota
+
+# This module incorporate different fractions of soil carbon, processes associated
+# with carbon input, and conversion between fractions, and major fluxes of carbon 
+# from the soil to carbon cycle model. Soil carbon will be divided into labile or
+# unprotected fractions and recalcitrant or protected fraction. The  labile 
+# fraction include the carbon from microbes (microbial biomass carbon), and easily 
+# decomposible plant residues (low C:N ratio). The recalcitrant fraction includes
+# carbon derived from residues with high C:N ratio, also carbon protected within
+# the clay complex are included under recalcitrant fraction (higher clay % in the
+# soil indicates greater protection from microbial decomposition). Decomposition 
+# is driven by soil microbes, and soil aggregation plays an important role in
+# protecting labile carbon from microbial access. Fungi have ability to decompose 
+# high C:N residues. Therefore, high fungi/bacteria ratio may promote decomposition
+# of stable carbon from soil. Decomposition of both labile and recalcitrant carbon
+# releases C to atmosphere as CO2.
+#  
+# In managed ecosystem (e.g., agricultural soil), cultivation practice like 
+# tillage break down the soil aggregates, thereby accelerating soil carbon loss 
+# due to exposure of protected C to microbial decomposition. Therefore, crop 
+# management practices such as no-till, residue retention,crop rotation, and 
+# cover cropping can help to stabilize the soil aggregates as well as provide 
+# additional carbon input. Crop rotation, and cover crops also increase 
+# below ground diversity which can change carbon turnover rate from the soil.
+# i also intend to include those management strategies in my soil carbon module.
+
+
+
+
+
+
+
+

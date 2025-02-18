1da3ba612277ad86419c6c6e025b5844112f0770
Azaj Mahmud <azmahmud@ttu.edu>
2025-02-18 12:03:15 -0600

    Added Rscript
    Added the description of the model in the script

---
+++ class_model/modules/module_mahmud/mahmud_FRCC.R
@@ -0,0 +1,20 @@
+# Fire regimes-Carbon Cycle Model
+# Azaj Mahmud
+
+#####################################################################################
+# Description
+#####################################################################################
+# The proposed model integrates carbon cycling and fire dynamics to simulate the
+# carbon budget using a mechanistic approach inspired by Akihiko Ito (2005).
+# The carbon cycle model consists of multiple carbon pools, capturing biomass accumulation, 
+# decomposition, and post-fire regrowth. The carbon pool, particularly the vegetation will 
+# be ecosystem specific if  this model expanded to eco-region or global scale. 
+# The fire regime model simulates fire ignition, spread, and extinction  based on mechanistic principles, 
+# explicitly modeling fire behavior as a function of plant traits, vegetation arrangement and weather condition, insipred by
+# Zylstra 2016 and 2018. This approach incorporates deterministic processes governing fire dynamics and fuel consumption 
+# including the fire occurence probability similarto Filipe Catry (2007). The interaction between 
+# the carbon and fire models allows for feedback mechanisms, where fuel availability influences fire intensity, 
+# and fire events regulate carbon fluxes through biomass burning, emissions, and post-fire vegetation recovery.
+# This framework provides a process-based understanding of the role of fire in shaping carbon dynamics under changing 
+# environmental conditions.
+#####################################################################################

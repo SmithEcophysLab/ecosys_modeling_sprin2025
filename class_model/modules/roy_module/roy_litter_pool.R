09cf2f263fd860e5bc5b95907e270f4968a36303
Puja Roy <puroy@ttu.edu>
2025-02-24 20:51:03 -0600

    I editet my R script, wrote an intial simple function

--- class_model/modules/roy_module/roy_litter_pool.R
+++ class_model/modules/roy_module/roy_litter_pool.R
@@ -5,5 +5,24 @@
 
 #I want to look at how the litter pool changes after drought in a particular
 #terrestrial ecosystem
-#Temperature, precipitation, dry biomass of litter can be the inputs
-# I am still not sure about the exact inputs and outputs
+#To start with a simple form of function, I am taking plant green and brown
+# biomass as input and their fraction as output
+
+## biomass: Given two numeric arguments gbm and lbm referring the mass of  
+## green biomass and litter biomass in gram
+##     Args:
+##       gbm and lbm = numeric argument 
+##     Returns:
+##      a numeric component representing the fraction of gbm and lbm
+
+biomass <- function(gbm = 200, lbm = 350){
+  fraction <- (gbm/lbm)
+  return(fraction)
+}
+
+# Example
+biomass()
+# [1] 0.5714286 #default
+biomass(400, 600)
+# [1] 0.6666667
+

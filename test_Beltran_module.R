ee8a38b11e95d92ae9195cedca11e07a5f3f4c14
ibeltr <ibeltran@ttu.edu>
2025-03-04 02:10:42 -0600

    Added log.md and test_Beltran_module.R for module updates and testing

---
+++ test_Beltran_module.R
@@ -0,0 +1,11 @@
+# test_Beltran_module.R
+## script to test the Beltran_competition_module.R script
+
+## load libraries
+library(R.utils)
+
+## load functions
+source('Beltran_competition_module.R')
+
+## run model
+li_under()

c349638dbb58c4cf65b13d14c4248e1b5c85f55e
Snehanjana Chatterjee <76224982+sneha294@users.noreply.github.com>
2025-03-25 14:29:50 -0500

    changed the function in .R file

--- class_model/modules/Chatterjee_module/chatterjee_Mycorrhizamodule.R
+++ class_model/modules/Chatterjee_module/chatterjee_Mycorrhizamodule.R
@@ -44,7 +44,38 @@ mycorrhizal_fungi_module <- function(carbon_fixed = 100,  # amount of carbon fix
 results <- mycorrhizal_fungi_module()
 
 
-
+Active_N_uptake <- function(npp = 100, # net primary productivity
+                            Cplant = 0.5 , # whole plant carbon
+                            Nplant = 0.6 , # whole plant nitrogen 
+                            Croot = 0.3 , # total carbon in root biomass
+                            Nsoil=  0.9, # available soil nitrogen
+                            Camf= 0.2,   # total carbon directed in AMF biomass
+                            Cacq = 0.3, # carbon available to spend on nitrogen acquisition
+                            kN = 1, # parameter that controls the cost as a function of soil N
+                            kC= 1) # parameter that controls the cost as a function of root C
+{ 
+  # Plant Nitrogen demand 
+  Ndemand <-  npp/(Cplant/Nplant)
+  
+  # Carbon cost to acquire nutrients from roots
+  COSTact <- (kN/Nsoil) * (kC/Croot)
+  
+  # Plant Nitrogen uptake through roots
+  Nuptake <- Cacq/COSTact
+  
+  # Carbon cost to acquire nutrients from AMF
+  COSTamf <- (kN/Nsoil)*(kC/Camf)
+  
+  # Plant Nitrogen uptake through AMF
+  Nuptakeamf <- Cacq/COSTamf
+  
+  # Total N uptake
+  TotalN <- COSTact + COSTamf
+  
+  results <- c(Ndemand,COSTact, Nuptake, COSTamf, Nuptakeamf, TotalN)
+  
+  return(results)
+}
 
 
 

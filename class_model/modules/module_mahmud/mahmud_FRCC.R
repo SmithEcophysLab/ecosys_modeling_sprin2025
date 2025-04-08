db96683ca86daac2b3d2f50543458f2cc79ec503
Azaj Mahmud <azmahmud@ttu.edu>
2025-04-08 12:31:43 -0500

    Updated model and performed sensitivity analysis
    Added sensitivity analysis script and updated the log file

--- class_model/modules/module_mahmud/mahmud_FRCC.R
+++ class_model/modules/module_mahmud/mahmud_FRCC.R
@@ -10,31 +10,14 @@
 #################################################################################################
 # Required Libraries
 ################################################################################################
-# we will fit the fire regimes as a function of environmental variables using gam similar 
-# to Pausas et al. (2022).
 
-
-FRCC_model <- function(years, flammability, vpd, moisture_loss_rate) {
-  carbon_stock <- 50  # Initial biomass, for now putting it as 50
-  results <- numeric(years)  # Store biomass over time
-  
-  for (year in 1:years) {
-    # Carbon increase over time
-    carbon_stock <- carbon_stock + 0  # Fixed yearly growth rate, need to determine but for now it's zero
-    
-    # Fire probability is directly proportional to VPD and moisture loss rate of the dominant plant species
-    fire_prob <- vpd[year] * moisture_loss_rate[year]  
-    
-    # Check for fire occurrence
-    if (runif(1) > fire_prob) { # this parts needs thought since fire occurence is not deterministic
-      fire_intensity <- flammability  # Fire intensity depends on the flammability of the most dominant plant species
-      carbon_stock <- carbon_stock * (1 - fire_intensity)  # Biomass loss due to fire
-    }
-    
-    results[year] <- carbon_stock
+FRCC_model <- function(carbon_stock = 0, moisture_loss_rate, vpd) {
+  vpd_normalized <- vpd / 5 # normalized the vpd
+  ignition_probability <- moisture_loss_rate * vpd_normalized
+  fire_occurs <- rbinom(1, 1, ignition_probability) # weighted probability of fire occurence
+  if (fire_occurs == 1) {
+    biomass_loss <- (1 - moisture_loss_rate) 
+    carbon_stock <- carbon_stock * (1 - biomass_loss)
   }
-  
-  return(results)
+  return(carbon_stock)
 }
-
-

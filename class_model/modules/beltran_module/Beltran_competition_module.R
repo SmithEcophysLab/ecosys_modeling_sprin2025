6d4c12f130738ea71a501f9fcbbf5268ce5fe7e8
ibeltr <ibeltran@ttu.edu>
2025-04-22 04:44:14 -0500

    updates to my model

--- class_model/modules/beltran_module/Beltran_competition_module.R
+++ class_model/modules/beltran_module/Beltran_competition_module.R
@@ -79,9 +79,26 @@ plant_competition <- function(TB = 100, # Total biomass (g)
                               N = 0.1, 
                               P = 0.1, 
                               K = 0.1,  # % C, N, P, K in biomass
-                              env_type = "moderate", # "dry", "moderate", "wet"
-                              light_lvl = "medium" # "low", "medium", "high"
+                              moisture = 0.50,
+                              light = 2000
 ) {
+  # Soil moisture categories
+  env_type <- ifelse(moisture <= 0.30, "dry",
+              ifelse(moisture <= 0.70, "moderate", "wet"))
+  
+  # Light levels categories
+  light_lvl <- ifelse(light <= 200, "low",
+           ifelse(light <= 600, "medium", "high"))
+  
+  # Nutrient level classification
+  n_level <- ifelse(N <= 0.03, "low",
+                    ifelse(N <= 0.07, "moderate", "high"))
+  p_level <- ifelse(P <= 0.03, "low",
+                    ifelse(P <= 0.07, "moderate", "high"))
+  k_level <- ifelse(K <= 0.03, "low",
+                    ifelse(K <= 0.07, "moderate", "high"))
+  
+                     
   # Water competition
   w_params <- switch(env_type,
                      dry = list(rc = 0.6, rn = 0.2, rp = 0.1, rk = 0.7,
@@ -93,29 +110,53 @@ plant_competition <- function(TB = 100, # Total biomass (g)
                                      sc = 0.2, sn = 0.2, sp = 0.5, sk = 0.5,
                                      Re = 0.3),
                      wet = list(rc = 0.1, rn = 0.1, rp = 0.1, rk = 0.1,
-                                lc = 0.6, ln = 0.7, lp = 0.2, lk = 0.1,
-                                sc = 0.3, sn = 0.2, sp = 0.7, sk = 0.1,
-                                Re = 0.2)
-  )
+                                lc = 0.6, ln = 0.7, lp = 0.2, lk = 0.2,
+                                sc = 0.3, sn = 0.2, sp = 0.7, sk = 0.7,
+                                Re = 0.2))
   
   # Light competition
   l_params <- switch(light_lvl,
                      low = list(lc = 1.3, sc = 1.5, rc = 0.7),
                      medium = list(lc = 1.0, sc = 1.0, rc = 1.0),
-                     high = list(lc = 0.8, sc = 0.7, rc = 1.2)
-  )
+                     high = list(lc = 0.8, sc = 0.7, rc = 1.2))
+  
+  # Nutrient-based carbon allocation modifiers
+  n_params <- switch(n_level,
+                     low = list(rc = 1.2, lc = 0.9, sc = 0.9),
+                     moderate = list(rc = 1.0, lc = 1.0, sc = 1.0),
+                     high = list(rc = 0.8, lc = 1.1, sc = 1.1))
+                     
+  p_params <- switch(p_level,
+                     low = list(rc = 1.2, lc = 0.9, sc = 0.9),
+                     moderate = list(rc = 1.0, lc = 1.0, sc = 1.0),
+                     high = list(rc = 0.8, lc = 1.1, sc = 1.1))
+                     
+  k_params <- switch(k_level,
+                     low = list(rc = 1.2, lc = 0.9, sc = 0.9),
+                     moderate = list(rc = 1.0, lc = 1.0, sc = 1.0),
+                     high = list(rc = 0.8, lc = 1.1, sc = 1.1))
+                     
   
   # C adjustments to organs after light competition
   w_params$rc <- w_params$rc * l_params$rc
   w_params$lc <- w_params$lc * l_params$lc
   w_params$sc <- w_params$sc * l_params$sc
   
+  # Nutrient-based adjustments to carbon allocation
+  nutrient_rc <- mean(c(n_params$rc, p_params$rc, k_params$rc))
+  nutrient_lc <- mean(c(n_params$lc, p_params$lc, k_params$lc))
+  nutrient_sc <- mean(c(n_params$sc, p_params$sc, k_params$sc))
+  
+  w_params$rc <- w_params$rc * nutrient_rc
+  w_params$lc <- w_params$lc * nutrient_lc
+  w_params$sc <- w_params$sc * nutrient_sc
+  
   # Calculate total remain C based on the input of biomass 
   total_C <- TB * C                 # C assimilated from photosynthesis
   lost_C <- total_C * w_params$Re   # Carbon lost to respiration
   remaining_C <- total_C - lost_C   # Carbon available for growth
   
-  # Calculate the adjust C after light competition 
+  # Calculate the adjust C after light and nutrient interaction levels
   TC <- w_params$rc + w_params$lc + w_params$sc
   RC <- remaining_C * (w_params$rc / TC)
   LC <- remaining_C * (w_params$lc / TC)
